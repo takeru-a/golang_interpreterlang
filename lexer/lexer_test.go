709aaf6c84378438814852fd9d25f656db4afe67
takeru-a <pypy20191022@gmail.com>
2022-11-22 16:43:29 +0900

    add:コードの字句解析

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -32,13 +32,45 @@ func (l *Lexer) readChar(){
 	l.readPosition += 1
 }
 
+func (l *Lexer) readIdentifier ()string{
+	position := l.position
+	for isLetter(l.ch){
+		l.readChar()
+	}
+	return l.input[position: l.position]
+}
+
+// 空白を読み飛ばす
+func (l *Lexer) skipWhitespace(){
+	for l.ch == ' ' || l.ch =='\t' || l.ch == '\n' || l.ch == '\r'{
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string{
+	position:= l.position
+	for isDigit(l.ch){
+		l.readChar()
+	}
+	return l.input[position: l.position]
+}
+
 func newToken(tokenType token.TokenType, ch byte)token.Token{
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
+func isLetter(ch byte) bool{
+	// ASCII Code 文字の定義
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool{
+	return '0' <= ch && ch <= '9'
+}
+
 func (l *Lexer) NextToken() token.Token{
 	var tok token.Token
-
+	l.skipWhitespace()
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -59,26 +91,72 @@ func (l *Lexer) NextToken() token.Token{
 	case 0:
 		tok.Literal =""
 		tok.Type  = token.EOF
+	default:
+		if isLetter(l.ch){
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		}else if isDigit(l.ch){
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		}else{
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
 }
 
 func TestNextToken(t *testing.T){
-	input := `=+(){},;`
+	input := `let a = 5;
+			  let b = 10;
+			  let add = fn(x, y){
+				x + y;
+			  };
+			  let result = add(a, b);
+			  `
 
 	tests := []struct{
 		//期待する型・文字
 		expectedType token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.INDENT, "a"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.INDENT, "b"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.INDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
+		{token.INDENT, "x"},
 		{token.COMMA, ","},
+		{token.INDENT, "y"},
+		{token.RPAREN, ")"},	
+		{token.LBRACE, "{"},
+		{token.INDENT, "x"},
+		{token.PLUS, "+"},
+		{token.INDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.INDENT, "result"},
+		{token.ASSIGN,"="},
+		{token.INDENT, "add"},
+		{token.LPAREN, "("},
+		{token.INDENT, "a"},
+		{token.COMMA, ","},
+		{token.INDENT, "b"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
