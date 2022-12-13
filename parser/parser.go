ab6a5ce3ac06eb009464d84a76ff3b5492b06674
takeru-a <pypy20191022@gmail.com>
2022-12-13 17:16:40 +0900

    add:return文追加

--- parser/parser.go
+++ parser/parser.go
@@ -56,6 +56,8 @@ func (p *Parser) parseStatement() ast.Statement{
 	switch p.curToken.Type{
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -99,4 +101,17 @@ func (p *Parser)expectPeek(t token.TokenType) bool{
 		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement{
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+    // セミコロンまで飛ばす
+	for !p.curTokenIs(token.SEMICOLON){
+		p.nextToken()
+	}
+
+	return stmt
 }
