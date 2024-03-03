717b0f57539abc2df65b22edfecc4bb9d590390f
takeru-a <pypy20191022@gmail.com>
2024-03-03 22:38:45 +0900

    基本機能実装完了

--- object/object.go
+++ object/object.go
@@ -1,7 +1,11 @@
 package object
 
-import(
+import (
+	"bytes"
 	"fmt"
+	"strings"
+
+	"github.com/takeru-a/golang_interpreterlang/ast"
 )
 
 type ObjectType string
@@ -10,6 +14,9 @@ const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
 	NULL_OBJ = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ = "ERROR"
+	FUNCTION_OBJ = "FUNCTION"
 )
 
 type Object interface {
@@ -38,3 +45,45 @@ type NULL struct{}
 
 func (n *NULL) Type() ObjectType { return NULL_OBJ }
 func (n *NULL) Inspect() string { return "null" }
+
+// Return
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+// Error
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string { return "ERROR: " + e.Message }
+
+// Function
+type Function struct {
+	Parameters []*ast.Indetifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+
+func (fn *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (fn *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fn.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(fn.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
